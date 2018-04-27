1c0a259a2889e28078c4f250b9f8b7cf971cf841
v1doq <kanderovd@gmail.com>
2018-04-27 16:38:47 +0300

    te

---
+++ Less_5_task_2.py
@@ -0,0 +1,10 @@
+class Room(a, b):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def p(self, a, b):
+        return a ** 2 + b ** 2
+
+    def s(self, a, b):
+        return a * b
